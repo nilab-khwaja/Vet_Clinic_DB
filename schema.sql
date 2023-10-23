0aac8d3253efa9af9f0024a4dc5467ce300d5de0
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-10-23 20:29:27 +0430

    modify schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 create database vet_clinic;
 create table animals(id SERIAL PRIMARY KEY, name VARCHAR(255), 
 date_of_birth DATE, escape_attempts INTEGER, neutered BOOLEAN, weight_kg DECIMAL );
+ALTER TABLE animals ADD species VARCHAR(50);
