e896b40445d77c0a550018cc38de7d51bcd5345d
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-10-25 12:04:29 +0430

    add tables and insert data into it

--- schema.sql
+++ schema.sql
@@ -9,3 +9,8 @@ CREATE TABLE species(id SERIAL PRIMARY KEY, name VARCHAR(50), );
 ALTER TABLE animals DROP COLUMN IF EXISTS species;
 ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
+
+
+CREATE TABLE vets(id SERIAL PRIMARY KEY, name VARCHAR(50), age INT, date_of_graduation DATE );
+CREATE TABLE specializations(vet_id INT, species_id INT, PRIMARY KEY (vet_id, species_id) , FOREIGN KEY (vet_id) REFERENCES vets(id), FOREIGN KEY(species_id) REFERENCES species(id));
+CREATE TABLE visits(animal_id INT, vet_id INT, visit_date DATE, FOREIGN KEY(animal_id) REFERENCES animals(id), FOREIGN KEY (vet_id) REFERENCES vets(id));
