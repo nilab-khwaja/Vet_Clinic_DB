07d6a8f987e22f6eebca21007c614db16f49c13d
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-10-30 13:47:57 +0430

    alter table owner and insert data into visits and owners table

--- schema.sql
+++ schema.sql
@@ -14,3 +14,7 @@ ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
 CREATE TABLE vets(id SERIAL PRIMARY KEY, name VARCHAR(50), age INT, date_of_graduation DATE );
 CREATE TABLE specializations(vet_id INT, species_id INT, PRIMARY KEY (vet_id, species_id) , FOREIGN KEY (vet_id) REFERENCES vets(id), FOREIGN KEY(species_id) REFERENCES species(id));
 CREATE TABLE visits(animal_id INT, vet_id INT, visit_date DATE, FOREIGN KEY(animal_id) REFERENCES animals(id), FOREIGN KEY (vet_id) REFERENCES vets(id));
+
+
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
